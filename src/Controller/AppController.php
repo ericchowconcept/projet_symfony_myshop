f730899875fbf9754287d9a01de3c80c1d20692a
ericchow8@gmail.com <ericchow8@gmail.com>
2023-06-22 12:19:42 +0200

    update day 2 morning

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Service\CartService;
 use App\Repository\ProduitRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AppController extends AbstractController
 {
@@ -16,5 +19,37 @@ class AppController extends AbstractController
         return $this->render('app/index.html.twig', [
             'produits' => $produits,
         ]);
+    }
+
+    
+    
+    #[Route('/cart/add/{id}', name:'cart_add')]
+    public function add($id, CartService $cs)
+    {
+        
+        $cs->add($id);
+        return $this->redirectToRoute('home');
+    }
+
+    #[Route('cart/remove/{id}', name:'cart_remove')]
+    public function remove($id, CartService $cs)
+    {
+        $cs->remove($id);
+        return $this->redirectToRoute('cart');
+
+    }
+
+    #[Route('/cart', name:'cart')]
+    public function show(CartService $cs)
+    {
+        
+        $cartWithData = $cs->cartWithData();
+        $total = $cs->total();
+
+        return $this->render('app/cart.html.twig', [
+            'items' => $cartWithData,
+            'total' => $total
+        ]);
+    
     }
 }
---
+++ src/Service/CartService.php
@@ -0,0 +1,95 @@
+<?php 
+
+namespace App\Service;
+
+use App\Repository\ProduitRepository;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class CartService 
+{
+    private $repo;
+
+    private $rs;
+
+    public function __construct(ProduitRepository $repo, RequestStack $rs)
+    {
+        $this->rs = $rs;
+        $this->repo = $repo;
+    }
+
+    public function add($id)
+    {
+        $session = $this->rs->getSession();
+        $cart = $session->get('cart',[]);
+
+        $qt = $session->get('qt', 0);
+
+
+        if(!empty($cart[$id]))
+        {
+            $cart[$id]++;
+            $qt++;
+        }else{
+            $cart[$id]=1;
+            $qt++;
+
+        }
+
+        $session->set('qt', $qt); 
+        $session->set('cart', $cart);
+    }
+
+    public function remove($id)
+    {
+        $session = $this->rs->getSession();
+        $cart = $session->get('cart',[]);
+
+        $qt = $session->get('qt', 0);
+
+        if(!empty($cart[$id]))
+        {
+            $qt -= $cart[$id];
+            unset($cart[$id]);
+        }
+
+        if($qt<0)
+        {
+            $qt =0;
+        }
+
+        $session->set('qt', $qt);
+        $session->set('cart', $cart);
+    }
+
+    public function cartWithData()
+    {
+        $session = $this->rs->getSession();
+        $cart = $session->get('cart',[]);
+
+        $cartWithData = [];
+
+        foreach($cart as $id => $quantity)
+        {
+            $produits = $this->repo->find($id);
+            $cartWithData[] = [
+                'produit' => $produits,
+                'quantity'=> $quantity
+            ];
+        }
+
+        return $cartWithData;
+    }
+
+    public function total()
+    {
+        $cartWithData = $this->cartWithData();
+        $total = 0;
+
+        foreach($cartWithData as $item)
+        {
+            $total += $item['produit']->getPrix() * $item['quantity'];
+        }
+
+        return $total;
+    }
+}
